384e71eadac7822741e35d46c3c08433a5d2123f
Thalita <thalita.macari@gmail.com>
2020-02-11 14:42:20 -0300

    aaaaa

--- app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -76,17 +76,17 @@ public final class R {
         public static final int bottom = 0x7f07002a;
         public static final int end = 0x7f070048;
         public static final int gone = 0x7f07004f;
-        public static final int invisible = 0x7f07005d;
-        public static final int left = 0x7f07005f;
-        public static final int packed = 0x7f07006e;
-        public static final int parent = 0x7f07006f;
-        public static final int percent = 0x7f070071;
-        public static final int right = 0x7f070075;
-        public static final int spread = 0x7f07008e;
-        public static final int spread_inside = 0x7f07008f;
-        public static final int start = 0x7f070094;
-        public static final int top = 0x7f0700a6;
-        public static final int wrap = 0x7f0700ac;
+        public static final int invisible = 0x7f07005e;
+        public static final int left = 0x7f070060;
+        public static final int packed = 0x7f07006f;
+        public static final int parent = 0x7f070070;
+        public static final int percent = 0x7f070072;
+        public static final int right = 0x7f070076;
+        public static final int spread = 0x7f07008f;
+        public static final int spread_inside = 0x7f070090;
+        public static final int start = 0x7f070095;
+        public static final int top = 0x7f0700a8;
+        public static final int wrap = 0x7f0700ae;
     }
     public static final class styleable {
         private styleable() {}
